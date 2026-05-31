dcbd3118994e0243b9a0221be9f9e8db88936896
roompartyyy-web <roompartyyy@gmail.com>
2026-05-31 05:14:48 +0100

    Import AddressPool model in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-require('dotenv').config();
+require('./models/AddressPool');
 
 // Routes
 const paymentRoutes = require('./routes/payment');
